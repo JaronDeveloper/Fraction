efd455244830de23132f03af06321a3fcd420c95
Prodject <Liter.Hari@gmail.com>
2023-09-26 18:58:26 +0200

    first commit

---
+++ Fraction/Fraction .cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_ = 0;
+	int denominator_ = 0;
+
+public:
+	Fraction() = default;
+
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;	
+		
+	}
+	bool operator == (Fraction other) {
+		int result = this->numerator_* this->numerator_+this->denominator_*this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result == result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator != (Fraction other) {
+		int result = this->numerator_ * this->numerator_ + this->denominator_ * this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result != result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator < (Fraction other) {
+		int result = this->numerator_ * this->numerator_ + this->denominator_ * this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result < result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator > (Fraction other) {
+		int result = this->numerator_ * this->numerator_ + this->denominator_ * this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result > result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator <= (Fraction other) {
+		int result = this->numerator_ * this->numerator_ + this->denominator_ * this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result <= result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator >= (Fraction other) {
+		int result = this->numerator_ * this->numerator_ + this->denominator_ * this->denominator_;
+		int result1 = other.numerator_ * other.numerator_ + other.denominator_ * other.denominator_;
+		if (result >= result1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+ 
+int main()
+{
+	Fraction f1(4, 3);	
+	Fraction f2(6, 11);
+	
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';	
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	
+	return 0;
+}
